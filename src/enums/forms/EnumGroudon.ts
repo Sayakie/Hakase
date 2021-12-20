95a337ada0234146e50c1adaf67a3f14ff4f28a3
Sayakie <sayakie@kakao.com>
2021-12-20 22:45:43 +0900

    Add ahead meta form for Groudon

--- src/enums/forms/EnumGroudon.ts
+++ src/enums/forms/EnumGroudon.ts
@@ -1,6 +1,7 @@
 import { EnumForm } from '@/enums/EnumForm.js'
+import { FormFlag } from '@/utils/Constants.js'
 
-const form = EnumForm.of('Groudon')
+const form = EnumForm.of('Groudon').addFlags(FormFlag.PinToPrefix)
 
 export class EnumGroudon {
   public static readonly Meta = form
