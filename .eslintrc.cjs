2b78f41cd58357843365df17d24ec1c631174a58
Sayakie <sayakie@kakao.com>
2022-04-12 01:28:08 +0900

    chore(lint): turn off the 'import/exports-last' rule
    Signed-off-by: Sayakie <sayakie@kakao.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -200,7 +200,6 @@ module.exports = {
       }
     ],
     'eslint-comments/disable-enable-pair': [OFF],
-    'import/exports-last': [ERROR],
     'import/first': [ERROR],
     'import/newline-after-import': [ERROR],
     'import/no-absolute-path': [ERROR],
