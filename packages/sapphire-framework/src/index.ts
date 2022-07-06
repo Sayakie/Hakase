2d85fc86ad434ec7336567833a8f7c5b8ffb2cc7
Sayakie <sayakie@kakao.com>
2022-07-06 23:39:59 +0000

    feat(sapphire-framework): support localizations

--- packages/sapphire-framework/src/index.ts
+++ packages/sapphire-framework/src/index.ts
@@ -1,2 +1,15 @@
+import type { LocalizationMap } from 'discord-api-types/v10'
+
 export * from '@sapphire/framework'
 export * from './Client.js'
+
+interface LocalizationProperties {
+  nameLocalizations: LocalizationMap | null
+  descriptionLocalizations: LocalizationMap | null
+}
+
+declare module '@sapphire/framework' {
+  interface Command extends LocalizationProperties {}
+
+  interface CommandOptions extends LocalizationProperties {}
+}
