9203b150cda44ace2e8b262a3d5023ffc4f1a844
Sayakie <sayakie@kakao.com>
2022-07-07 02:05:58 +0000

    feat(sapphire-framework): add localizable slash command

--- packages/sapphire-framework/src/index.ts
+++ packages/sapphire-framework/src/index.ts
@@ -1,15 +1,3 @@
-import type { LocalizationMap } from 'discord-api-types/v10'
-
-export * from '@sapphire/framework'
 export * from './Client.js'
-
-interface LocalizationProperties {
-  nameLocalizations: LocalizationMap | null
-  descriptionLocalizations: LocalizationMap | null
-}
-
-declare module '@sapphire/framework' {
-  interface Command extends LocalizationProperties {}
-
-  interface CommandOptions extends LocalizationProperties {}
-}
+export * from './structures/Command.js'
+export * from '@sapphire/framework'
