f6556e7469a883afb3c13249f24c1e3ae2150b0c
Sayakie <sayakie@kakao.com>
2022-07-09 19:59:47 +0000

    fix(constant): root dir encounter unknown path

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,9 +1,10 @@
+import { join } from 'node:path'
 import { URL } from 'node:url'
 
-const root = new URL(`../../../`, import.meta.url)
+const root = new URL(`../`, import.meta.url)
 
 export const Directories = {
-  Data: new URL(`data`, root),
+  Data: new URL(join(`data`), root),
   Root: root
 }
 
