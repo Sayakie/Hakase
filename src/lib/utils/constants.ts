b794ab6a573cea679f621ca5178f40d0f00955ca
Sayakie <sayakie@kakao.com>
2022-07-09 16:38:30 +0000

    feat: add resourceIdentifiers

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -6,3 +6,10 @@ export const Directories = {
   Data: new URL(`data`, root),
   Root: root
 }
+
+export const enum ResourceIdentifiers {
+  // Main resource head
+  MINECRAFT = `minecraft`,
+  PIXELMON = `pixelmon`,
+  FORGE = `forge`
+}
