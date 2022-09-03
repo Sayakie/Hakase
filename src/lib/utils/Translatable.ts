4be4d5644cb262225d3c6197889f029d5dbc44d7
Sayakie <sayakie@kakao.com>
2022-09-03 18:32:42 +0900

    feat: add `Translatable`

---
+++ src/lib/utils/Translatable.ts
@@ -0,0 +1,12 @@
+import type { Nullish } from '@sapphire/utilities'
+import type { Locale } from 'discord-api-types/v10'
+
+export interface Translatable {
+  translation(): Translation
+}
+
+export interface Translation {
+  key(): string
+
+  with(locale: `${Locale}`): string | Nullish
+}
