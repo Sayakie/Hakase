c9882cfa25bfeb6c0d8220438daf8a3c4bd98236
Sayakie <sayakie@kakao.com>
2022-09-07 16:30:07 +0900

    fix: try json parse twice

--- src/lib/inject/statInjector.ts
+++ src/lib/inject/statInjector.ts
@@ -8,18 +8,7 @@ import { loadAllStats } from '#lib/utils/loaders.js'
 export async function injectStats(): Promise<void> {
   const stats = await loadAllStats()
 
-  for (const [pokemonName, raw] of stats) {
-    const result = Result.from(() => JSON.parse(raw) as Stat)
-
-    const data = result.match({
-      err: () => null,
-      ok: data => data
-    })
-
-    if (isNullish(data)) {
-      continue
-    }
-
+  for (const [pokemonName, data] of stats) {
     const species = Reflect.construct(PokemonSpecies, [data])
 
     Reflect.set(PokemonSpecies, toTitleCase(pokemonName), species)
